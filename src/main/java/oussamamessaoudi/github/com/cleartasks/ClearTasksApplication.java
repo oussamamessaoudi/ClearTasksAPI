e8a572b0c2855bc9957e9d857018df113f535bf0
oussamamessaoudi <oussm.messaoudi@gmail.com>
2019-10-14 21:06:59 +0100

    Initial commit

---
+++ src/main/java/oussamamessaoudi/github/com/cleartasks/ClearTasksApplication.java
@@ -0,0 +1,11 @@
+package oussamamessaoudi.github.com.cleartasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearTasksApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClearTasksApplication.class, args);
+    }
+}
